80207e4f301d682f84bfa5af3a026ba0b3f31679
mariam-fouad <mariam.fouad94@gmail.com>
2018-10-09 14:51:00 +0300

    create basic User containers

---
+++ src/containers/User.js
@@ -0,0 +1,14 @@
+import react,{Component} from 'react';
+
+class Users extends Component{
+    render(){
+        return (
+            <div>
+                <h1>Users</h1>
+                <p>All users love Pizaa!</p>
+            </div>
+        );
+    }
+}
+
+export default Users;
