edc0896c829147c2c8ccb019077d12520d5b762f
mariam-fouad <mariam.fouad94@gmail.com>
2018-10-09 21:30:53 +0300

    fix a spelling error

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const autoprefixer = reqire ('autoprefixer');
+const autoprefixer = require ('autoprefixer');
 
 module.exports={
     devtool:'cheap-module-eval-source-map',
