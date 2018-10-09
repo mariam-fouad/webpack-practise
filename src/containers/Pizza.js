79d523b42bb7c48b75c761c67001e5f3c2030d71
mariam-fouad <mariam.fouad94@gmail.com>
2018-10-09 15:03:24 +0300

    import the PizzaImage component to the Pizza container

--- src/containers/Pizza.js
+++ src/containers/Pizza.js
@@ -1,9 +1,12 @@
 import react,{Component} from 'react';
-
+import PizzaImage from '../components/PizzaImage/PizzaImage';
 class Pizza extends Component{
     render(){
         return (
-
+            <div>
+                <h1>Pizza</h1>
+                <PizzaImage/>
+            </div>
         );
     }
 }
