0a02cf0dd448d63e0abe37547c02915564e95abc
mariam-fouad <mariam.fouad94@gmail.com>
2018-10-09 15:12:32 +0300

    add logic in the App.js

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React,{Component} from 'react';
+import {Link,Route } from 'react-router-dom';
+
+import User from './containers/User';
+import asyncComponent from '/hoc/asyncComponent';
+
+const asycPizza = asyncComponent(()=>{
+    return import('./containers/Pizza');
+})
+class App extends Component{
+    render() {
+        return(
+            <div>
+                <div>
+                    <Link to='/' exact >Users</Link>
+                    <Link to='/pizza'>Pizza</Link>
+                </div>
+                <div>
+                    <Route to='/' component={User}/>
+                    <Route to='/pizza' component={asycPizza}/>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
